aa938190163f13953bc1719d1d86bd076cdbe3fc
Dishant <dishantpatel098@gmail.com>
2021-09-17 00:49:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require("path")
+
+const app = express()
+
+app.use('/static',express.static(path.resolve(__dirname,"static")));
+
+app.get('/*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"index.html"))
+})
+app.listen(5000,()=>{
+    console.log('Server is listening....')
+})
